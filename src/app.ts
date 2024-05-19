bb692b19f1dc6e51192ae213698f408ce581c437
Rafael <rafaelmbeleboni@gmail.com>
2024-05-19 20:48:37 -0300

    feat: ready to deploy

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,39 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import mainRoutes from './routes/main.routes'; // Importa as rotas principais
 
 const app = express();
 
-const corsOptions = {
-    origin: 'https://projeto-fullstack-barbearia.vercel.app', // Substitua esta URL pela URL do seu front-end
-    optionsSuccessStatus: 200 // Alguns navegadores legados necessitam deste status
-};
-
-app.use(cors(corsOptions));
+// Middleware para configurar CORS para permitir todas as origens
+app.use((req: Request, res: Response, next: NextFunction) => {
+    // Permite requisições de qualquer origem
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    
+    // Define métodos HTTP permitidos para CORS
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS, CONNECT, TRACE"
+    );
+    
+    // Define headers permitidos em requisições CORS
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+  
+    // Permite o envio de cookies e credenciais em requisições cross-origin
+    // Nota: Quando o `Access-Control-Allow-Origin` é configurado para '*', o `Access-Control-Allow-Credentials` deve ser 'false'.
+    res.setHeader("Access-Control-Allow-Credentials", "false");
+    
+    // Permite requisições de redes privadas
+    res.setHeader("Access-Control-Allow-Private-Network", "true");
+    
+    // Define o tempo máximo em segundos que a resposta ao preflight request pode ser cacheada
+    res.setHeader("Access-Control-Max-Age", "7200");
+  
+    next();
+  });
+  
 
 app.use(express.json());
 
