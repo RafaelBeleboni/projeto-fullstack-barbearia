50555c300d4fd0e5e1b55d5a6b73bcd6825a9262
Rafael <rafaelmbeleboni@gmail.com>
2024-05-19 20:32:38 -0300

    feat: ready to deploy

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,13 @@ import mainRoutes from './routes/main.routes'; // Importa as rotas principais
 
 const app = express();
 
-// Middleware para CORS e JSON
-app.use(cors());
+const corsOptions = {
+    origin: 'https://projeto-fullstack-barbearia.vercel.app', // Substitua esta URL pela URL do seu front-end
+    optionsSuccessStatus: 200 // Alguns navegadores legados necessitam deste status
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // Use o roteador principal para gerenciar rotas
