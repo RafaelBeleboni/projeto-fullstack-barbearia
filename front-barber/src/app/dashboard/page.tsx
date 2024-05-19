f54846c8d4e27144cad773dd324846be014f32ab
Rafael <rafaelmbeleboni@gmail.com>
2024-05-19 19:58:29 -0300

    fix: localstorage

--- front-barber/src/app/dashboard/page.tsx
+++ front-barber/src/app/dashboard/page.tsx
@@ -1,36 +1,37 @@
-"use client"
-
+'use client'
 import apiBase from '@/api/apiBase';
 import ScheduleCalendar from '@/components/Dashboard/ScheduleCalendar';
 import Spinner from '@/components/Spinner';
 import { useRouter } from 'next/navigation'; // Use 'next/navigation' em vez de 'next/router'
 import React, { useEffect, useState } from 'react';
 
-
 const DashboardPage = () => {
-  const router = useRouter()
-  const [ isLoading, setIsLoading ] = useState(true)
-  const client = localStorage.getItem("@Cliente") || ""
-  const token = localStorage.getItem("@Token") || ""
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
-useEffect(()=>{
+  useEffect(() => {
+    const client = typeof window !== 'undefined' ? localStorage.getItem("@Cliente") || "" : "";
+    const token = typeof window !== 'undefined' ? localStorage.getItem("@Token") || "" : "";
+
     const verifyToken = async () => {
-      try{
-        await apiBase.post("/auth/verificar-token", { token: token})
-        setIsLoading(false)
-      }catch(err){
-        router.push("/")
+      try {
+        if (token) {
+          await apiBase.post("/auth/verificar-token", { token: token });
+          setIsLoading(false);
+        } else {
+          router.push("/");
+        }
+      } catch (err) {
+        router.push("/");
       }
-    }
-    
-verifyToken()
-},[])
+    };
 
+    verifyToken();
+  }, [router]);
 
-return (
+  return (
     <>
-    
-    {isLoading ? <Spinner /> : <ScheduleCalendar/>}
+      {isLoading ? <Spinner /> : <ScheduleCalendar />}
     </>
   );
 };
